f0417b5b346f30af56cc3b1bf52de0ea19977e59
KOUSHIK DEY <koushikdeynghs@gmail.com>
2026-04-22 23:35:44 +0600

    Subscribe / Unsubscribe to Vendors with Email Notification and Water Intake Tracker with depends on weather

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = create_app()
 
 if __name__ == "__main__":
 <<<<<<< HEAD
-    app.run(debug=True, host="127.0.0.1", port=1694)
+    app.run(debug=True)
 =======
     app.run(debug=True)
 >>>>>>> 6dae3acd52729935819cc50f9b4e5f1e759c8cd3
